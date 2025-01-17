8ded188e798d7b5dfc26a5acccc1caf12f06ad82
SerenityFlaim <phil_th@mail.ru>
2025-01-17 17:41:18 +0300

    EP|delete operation for client entity

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,5 @@
 class ClientsController < ApplicationController
-  before_action :set_client, only: %i[ show edit update ]
+  before_action :set_client, only: %i[ show edit update destroy ]
   
   def index
     @clients = Client.all
@@ -30,6 +30,11 @@ class ClientsController < ApplicationController
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @client.destroy
+    redirect_to clients_path
   end
 
   private
