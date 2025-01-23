66e6b84a1127576460e5444b89f5975f17ed2107
SerenityFlaim <phil_th@mail.ru>
2025-01-23 14:11:09 +0300

    EP| regexp issue fixed

--- app/models/client.rb
+++ app/models/client.rb
@@ -10,7 +10,7 @@ class Client < ApplicationRecord
   }
 
   validates :fullname, presence: true, format: {
-     with: /\A([A-Za-z]+(?: [A-Za-z]+){1,2})\z/, 
+     with: /\A([A-Za-zА-Яа-я]+(?: [A-Za-zА-Яа-я]+){1,2})\z/, 
      message: "must be two or three words without numbers" 
   }
 end
